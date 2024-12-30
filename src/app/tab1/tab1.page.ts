dedf31ea50945062e039195bf4b3247549461fa9
Diego Flores <dfflores@Diegos-Laptop.local>
2024-12-30 16:33:47 -0500

    lastCommit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,20 +1,33 @@
-import { Component, signal } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent,  IonFab, IonFabButton, IonIcon, IonCard, } from '@ionic/angular/standalone';
+import { Component, signal, ViewChild, ElementRef} from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent,  IonFab, IonFabButton, IonIcon, IonCard,   IonCardContent, IonButton, IonList, IonItem, IonLabel,} from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { addIcons } from 'ionicons';
 import { cloudUploadOutline } from 'ionicons/icons';
-
+import { TeachablemachineService } from '../services/teachablemachine.service';
+/* Importe el pipe */
+import { PercentPipe } from '@angular/common';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonFab, IonFabButton, IonIcon, IonCard, ],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonFab, IonFabButton, IonIcon, IonCard, IonCardContent, IonButton, IonList,PercentPipe ,IonItem, IonLabel,],
 })
 export class Tab1Page {
 
   imageReady = signal(false)
   imageUrl = signal("")
-  constructor() {
+
+ /* Declare la referencia al elemento con el id image */
+ @ViewChild('image', { static: false }) imageElement!: ElementRef<HTMLImageElement>;
+
+     /* Lista de predicciones */
+  		predictions: any[] = [];
+
+    /* Declare los atributos para almacenar el modelo y la lista de clases */
+    modelLoaded = signal(false);
+    classLabels: string[] = [];
+    /* Registre el servicio en el constructor */
+  constructor(private teachablemachine: TeachablemachineService) {
     addIcons({ cloudUploadOutline });
 
   }
@@ -36,6 +49,23 @@ export class Tab1Page {
         reader.readAsDataURL(file); // Leer el archivo como base64
     }
 }
+ /* Método ngOnInit para cargar el modelo y las clases */
+  async ngOnInit() {
+    await this.teachablemachine.loadModel()
+    this.classLabels = this.teachablemachine.getClassLabels()
+    this.modelLoaded.set(true)
+  }
+
+      /* Método para obtener la predicción a partir de la imagen */
+      async predict() {
+        try {
+            const image = this.imageElement.nativeElement;
+            this.predictions = await this.teachablemachine.predict(image);
+        } catch (error) {
+            console.error(error);
+            alert('Error al realizar la predicción.');
+        }
+    }
 
 
 }
