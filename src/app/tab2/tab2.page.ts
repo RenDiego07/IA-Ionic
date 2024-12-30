e6e9b7051573cf404535df8b9d31f4cb6e14ae80
Diego Flores <dfflores@Diegos-Laptop.local>
2024-12-29 23:55:24 -0500

    first

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,33 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent,      IonCard, IonCardHeader, IonCardTitle, IonCardContent,
+  IonSelect, IonSelectOption, IonTextarea,IonButton,
+  IonList, IonItem, IonLabel, } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { ReactiveFormsModule } from '@angular/forms';   // Este un módulo que permite crear y gestionar formularios reactivos 
+                                                        // permite la validación de campos
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonCard, IonCardHeader, IonCardTitle, IonCardContent,
+    IonSelect, IonSelectOption, IonTextarea,IonButton,
+    IonList, IonItem, IonLabel,ReactiveFormsModule,]
 })
 export class Tab2Page {
 
+  myForm: FormGroup = new FormGroup({
+    score: new FormControl("", Validators.required),
+    opinion: new FormControl("", Validators.required)
+  });
+  
+  onSubmit() {
+    console.log(this.myForm.value);
+    alert(this.myForm.controls["score"].value)
+    this.myForm.reset()
+}
   constructor() {}
 
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard,IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent,
+  ],
 })
+// no fue necesario agregar las clases importadas porque Ionic si las reconoce, a diferencia con la clase Icons que si se necesitó añadirla en el constructor de la clase
 export class Tab3Page {
   constructor() {}
 }
